d26f7c7047c590c8be30f4f3af7fac7598675f59
René Hansen <rene@hansen-audio.de>
2021-10-01 15:58:10 +0200

    Refactor shuffle note tests

--- src/detail/shuffle_note.rs
+++ src/detail/shuffle_note.rs
@@ -1,18 +1,16 @@
 // Copyright(c) 2021 Hansen Audio.
 
-use std::usize;
+use crate::Real;
 
-use crate::RealType;
-
-pub fn is_shuffle_note(node_index: usize, note_len: RealType) -> bool {
+pub fn is_shuffle_note(note_index: usize, note_len: Real) -> bool {
     if note_len == 1. / 16. {
-        return is_even(node_index + 1, 2);
+        return is_even(note_index + 1, 2);
     } else if note_len == 1. / 32. {
-        return is_even(node_index + 2, 4);
+        return is_even(note_index + 2, 4);
     } else if note_len == 1. / 64. {
-        return is_even(node_index + 4, 8);
+        return is_even(note_index + 4, 8);
     } else if note_len == 1. / 128. {
-        return is_even(node_index + 8, 16);
+        return is_even(note_index + 8, 16);
     } else {
         return false;
     }
@@ -28,55 +26,30 @@ fn is_even(value: usize, divider: usize) -> bool {
 
 #[cfg(test)]
 mod tests {
-    use crate::{detail::shuffle_note::is_shuffle_note, RealType};
+    use super::*;
 
     #[test]
     fn tests_is_shuffle_note_16() {
         let mut step_index = 0;
-        const NOTE_LEN: RealType = 1. / 16.;
-
-        assert_eq!(is_shuffle_note(step_index, NOTE_LEN), false);
-        step_index += 1;
-        assert_eq!(is_shuffle_note(step_index, NOTE_LEN), true);
-        step_index += 1;
-        assert_eq!(is_shuffle_note(step_index, NOTE_LEN), false);
-        step_index += 1;
-        assert_eq!(is_shuffle_note(step_index, NOTE_LEN), true);
-        step_index += 1;
-        assert_eq!(is_shuffle_note(step_index, NOTE_LEN), false);
-        step_index += 1;
-        assert_eq!(is_shuffle_note(step_index, NOTE_LEN), true);
-        step_index += 1;
-        assert_eq!(is_shuffle_note(step_index, NOTE_LEN), false);
-        step_index += 1;
-        assert_eq!(is_shuffle_note(step_index, NOTE_LEN), true);
+        const NOTE_LEN: Real = 1. / 16.;
+        const TEST_RESULTS: [bool; 8] = [false, true, false, true, false, true, false, true];
+        for r in TEST_RESULTS {
+            assert_eq!(is_shuffle_note(step_index, NOTE_LEN), r);
+            step_index += 1;
+        }
     }
 
     #[test]
     fn tests_is_shuffle_note_32() {
         let mut step_index = 0;
-        const NOTE_LEN: RealType = 1. / 32.;
+        const NOTE_LEN: Real = 1. / 32.;
+        const TEST_RESULTS: [bool; 11] = [
+            false, false, true, false, false, false, true, false, false, false, true,
+        ];
 
-        assert_eq!(is_shuffle_note(step_index, NOTE_LEN), false);
-        step_index += 1;
-        assert_eq!(is_shuffle_note(step_index, NOTE_LEN), false);
-        step_index += 1;
-        assert_eq!(is_shuffle_note(step_index, NOTE_LEN), true);
-        step_index += 1;
-        assert_eq!(is_shuffle_note(step_index, NOTE_LEN), false);
-        step_index += 1;
-        assert_eq!(is_shuffle_note(step_index, NOTE_LEN), false);
-        step_index += 1;
-        assert_eq!(is_shuffle_note(step_index, NOTE_LEN), false);
-        step_index += 1;
-        assert_eq!(is_shuffle_note(step_index, NOTE_LEN), true);
-        step_index += 1;
-        assert_eq!(is_shuffle_note(step_index, NOTE_LEN), false);
-        step_index += 1;
-        assert_eq!(is_shuffle_note(step_index, NOTE_LEN), false);
-        step_index += 1;
-        assert_eq!(is_shuffle_note(step_index, NOTE_LEN), false);
-        step_index += 1;
-        assert_eq!(is_shuffle_note(step_index, NOTE_LEN), true);
+        for r in TEST_RESULTS {
+            assert_eq!(is_shuffle_note(step_index, NOTE_LEN), r);
+            step_index += 1;
+        }
     }
 }
